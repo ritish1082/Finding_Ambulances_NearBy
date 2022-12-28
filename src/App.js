51a7629df3ac91e032af95234e1e621de4497152
ritish <sritish.10@gmail.com>
2022-12-28 22:09:45 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,71 +1,45 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import AmbulanceDetailsUI from './Components/AmbuDetailsUI';
-// import { Routes, Route, Link } from 'react-router-dom';
-// import './Components/Intee.css'
-// function App() {
-//   return (
-    
-//     <div className='intee mt-0'>
-      
-//       <AmbulanceDetailsUI/>
-      
-
-//     </div>
-    
-    
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import './App.css';
-import {Navbar, Container, Nav, OverlayTrigger, Tooltip} from 'react-bootstrap';
-import Home from './Components/Home'
-// import Profile from './pages/Profile'
-import AmbuDetailsUI from './Components/AmbuDetailsUI';
-import HospiDetailsUI from './Components/HospiDetailsUI';
-import { Routes, Route, Link } from 'react-router-dom';
-// import {Routes, Route} from 'react-router-dom';
+import "./App.css";
+import {
+  Navbar,
+  Container,
+  Nav
+} from "react-bootstrap";
+import Home from "./Components/Home";
+import AmbuDetailsUI from "./Components/AmbuDetailsUI";
+import HospiDetailsUI from "./Components/HospiDetailsUI";
+import { Routes, Route } from "react-router-dom";
 import "./Components/Intee.css";
 
 function App() {
-  
   return (
-    <div className='intee'>
-
-      {/* Creating the Navbar */} 
-      <Navbar collapseOnSelect expand="lg" className='bg-dark' variant="dark" style={{zIndex: "1"}}>
+    <div className="intee">
+      {/* Creating the Navbar */}
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="bg-dark"
+        variant="dark"
+        style={{ zIndex: "1" }}
+      >
         <Container>
-          <Navbar.Brand href="/" className='text-white'>
-            <b style={{fontFamily: "monospace", fontSize: "xx-large"}}> Life Networks</b>
+          <Navbar.Brand href="/" className="text-white">
+            <b style={{ fontFamily: "monospace", fontSize: "xx-large" }}>
+              {" "}
+              Life Networks
+            </b>
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className='ms-auto'>
-              <Nav.Link href="/" className='home text-white'>Home</Nav.Link>
-              <Nav.Link href="/ambudetails" className='collab text-white'>Ambulance Services</Nav.Link>
-              <Nav.Link href="/hospitaldetails" className='forum text-white'>Hospital Services</Nav.Link>
-              
-                
-              
+            <Nav className="ms-auto">
+              <Nav.Link href="/" className="home text-white">
+                Home
+              </Nav.Link>
+              <Nav.Link href="/ambudetails" className="collab text-white">
+                Ambulance Services
+              </Nav.Link>
+              <Nav.Link href="/hospitaldetails" className="forum text-white">
+                Hospital Services
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -74,14 +48,11 @@ function App() {
       {/* These are the routes for moving across the pages */}
       <Routes>
         <Route path="" element={<Home />} />
-        <Route path="/ambudetails" element={<AmbuDetailsUI/>} />
+        <Route path="/ambudetails" element={<AmbuDetailsUI />} />
         <Route path="/hospitaldetails" element={<HospiDetailsUI />} />
-        
       </Routes>
     </div>
   );
 }
 
 export default App;
-
-
